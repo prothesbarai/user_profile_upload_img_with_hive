2ff9eb2ea948e94c5087811e9277cf64723609a5
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-08-17 17:48:52 +0600

    sp

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -125,9 +125,9 @@ class _HomePageState extends State<HomePage> {
                   left: 20,
                   child: CircleAvatar(
                     radius: 60,
-                    backgroundColor: (imageProvider.profileImages == null) ? Color(0xff1f2b3b) : (profileImage == null) ? Color(0xff1f2b3b) : null,
+                    backgroundColor: (imageProvider.profileImages == null && profileImage == null) ? Color(0xff1f2b3b) : null,
                     backgroundImage: (imageProvider.profileImages != null) ? FileImage(imageProvider.profileImages!) : (profileImage != null) ? FileImage(profileImage!) : null,
-                    child: ( imageProvider.profileImages == null && profileImage == null) ? Icon(Icons.person, size: 50, color: Colors.white) : null,
+                    child: (imageProvider.profileImages == null && profileImage == null) ? Icon(Icons.person, size: 50, color: Colors.white) : null,
                   )
               ),
 
