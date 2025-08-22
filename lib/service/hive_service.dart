8d328d06de97ad752b45437ef92294ca6dfae04c
prothesbarai <aspprothesbarai1619@gmail.com>
2025-08-22 09:36:07 +0600

    main

--- lib/service/hive_service.dart
+++ lib/service/hive_service.dart
@@ -1,10 +1,19 @@
-import 'package:hive/hive.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:path_provider/path_provider.dart';
-
 class HiveService {
   static Future<void> initHive() async{
-    var dir = await getApplicationDocumentsDirectory();
-    Hive.init(dir.path);
-    await Hive.openBox("StoreUserImages");
+    /*
+      if অংশ Web এর জন্য Hive initialize করা হচ্ছে। Web এ কোনো ফাইল সিস্টেম নেই, তাই Hive internally browser এর localStorage ব্যবহার করে ডেটা সংরক্ষণ করে।
+      if অংশ শুধুমাত্র Web প্ল্যাটফর্মে execute হবে। .... else  অংশ অংশ Mobile এবং Desktop App এর জন্য
+    */
+    if (kIsWeb) {
+      await Hive.initFlutter();
+    } else {
+      var dir = await getApplicationDocumentsDirectory();
+      Hive.init(dir.path);
+      await Hive.openBox("StoreUserImages");
+    }
   }
+
 }
